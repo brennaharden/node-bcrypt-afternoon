24e370e29c094661d5f2ec4bfdf6e928174c244c
Brenna Harden <jupitereyes@Brennas-MacBook-Air.local>
2020-08-17 15:25:36 -0600

    completed through step 7

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const session = require('express-session');
 const massive = require('massive');
 const authCtrl = require('./controllers/authController');
 const treasureCtrl = require('./controllers/treasureController');
+const auth = require('./middleware/authMiddleware');
 
 const app = express();
 const PORT = 4000;
@@ -33,6 +34,8 @@ app.post('/auth/register', authCtrl.register);
 app.post('/auth/login', authCtrl.login);
 app.get('/auth/logout', authCtrl.logout);
 app.get('/api/treasure/dragon', treasureCtrl.dragonTreasure);
+app.get('/api/treasure/user', auth.usersOnly, treasureCtrl.getUserTreasure);
+app.post('/api/treasure/user', auth.usersOnly, treasureCtrl.addUserTreasure)
 
 
 app.listen(PORT, () => {
